fdef3a07b638e70ae4629c169cd90bfa686e30b6
Pedro Henrique Belino <51128990+pbelino@users.noreply.github.com>
2019-06-14 10:46:02 -0300

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ app.get('/', (req, res)=>{
     res.status(200).send({message:'Olá mundo, estou funcionando'});
 })
 
+app.get('/ola', (req, res)=>{
+    res.status(200).send({message:'oi eu sou a rota ola'});
+})
+
 app.listen(3001, ()=>{
     console.log("API rodando no porta 3001");
 })
